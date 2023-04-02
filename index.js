193e115c7ed3e3361923f336634870e5a2b214dd
simonryrie <simonryrie@gmail.com>
2023-04-02 19:04:36 +0100

    Ghost sticker function added
    Relates #2

--- index.js
+++ index.js
@@ -11,4 +11,25 @@ function createButtons() {
     });
 }
 
-createButtons()
+function createGhostSticker() {
+    const ghostSticker = document.createElement('div');
+    ghostSticker.classList.add('ghost-sticker');
+    ghostSticker.style.fontSize = getComputedStyle(document.documentElement).getPropertyValue('--emoji-size');
+    ghostSticker.textContent = '';
+    document.body.appendChild(ghostSticker);
+  
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        ghostSticker.textContent = button.textContent;
+      });
+    });
+  
+    document.addEventListener('mousemove', (event) => {
+      ghostSticker.style.top = `${event.clientY - 47.5}px`;
+      ghostSticker.style.left = `${event.clientX - 47.5}px`;
+    });
+}
+  
+createGhostSticker();
+createButtons();
